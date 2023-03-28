73822b1fa4c2e3303cfdae99f0159615df2996da
Dinda <72862688+dindadl@users.noreply.github.com>
2023-03-28 17:32:48 +0700

    Add files via upload

---
+++ tentang.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width-device-width, intial-scale=1.0">
+
+<title>Sistem Pakar Diagnosa Penyakit Gigi dan Mulut Manusia </title>
+
+<link rel="stylesheet" href="style1.css">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Product Sans:wght@200;300;400;500;600&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+<div class="header">
+	<div class="container">
+	<div class="navbar">
+	
+	<nav>
+		<ul id="MenuItems">
+		<li><a href="index.php">Beranda</a></li>
+		<li><a href="daftarpenyakit.php">Daftar Penyakit</a></li>
+		<li><a href="konsultasi.php">Konsultasi</a></li>
+		<li><a href="tentang.php">Tentang</a></li>
+		
+		</ul>
+	</nav>	
+	<br></br>
+	<a href="admin/login.php" class="split"><img src="admins.png" width="20px" height="20px"></a>
+	</div>
+</div>
+</div>
+
+<div class="row">
+	<div class="col-1">
+	<h1>Tentang</h1>
+	<p>Berikut ini merupakan penjelasan dari mengapa Sistem Pakar ini dibuat,</p>
+	<br>
+	
+	<br>
+
+</div>
+</div>
+
+<div class="offer">
+	<div class="small-container">
+	<div class="row-3">
+	<div class="col-3">	
+</div>
+
+	<div class="col-3">
+	<p>Salah satu cabang dari kecerdasan buatan adalah pembuatan sebuah sistem pakar yang dapat membantu pasien dalam mengetahui dan mendiagnosa penyakit gigi dan mulut mereka. Dengan adanya sistem pakar tersebut, proses diagnosa penyakit dapat dilakukan dimana saja dan kapan saja, tanpa diharuskan untuk melakukan konsultasi langsung dengan tenaga medis. Berdasarkan uraian diatas, penulis bermaksud untuk melakukan penelitian dengan judul <b>“Implementasi Metode Forward Chaining pada Pembuatan Sistem Pakar Diagnosa Penyakit Gigi dan Mulut Manusia Berbasis Web”</b> dengan tujuan untuk membantu masyarakat dalam melakukan diagnosa penyakit gigi dan mulut yang mereka alami secara akurat dan meningkatkan tingkat kesadaran dalam pencegahan dini, perawatan, dan penanganan gangguan gigi dan mulut. </p>
+	<br>
+	
+	<a href="konsultasi.php"class="btn">Mulai Konsultasi &#8594;</a>
+
+</div>
+</div>
+</div>
+</div>
+
+
+
+<div class="footer">
+	<div class="container">
+	
+	<div class="footer-col-1">
+		<br></br>
+		<h3>Website by Dinda Dwi Laras</h3>
+		<p>All rights reserved.</p>
+		
+		</div>
+		</div>
+
+
+</body>
